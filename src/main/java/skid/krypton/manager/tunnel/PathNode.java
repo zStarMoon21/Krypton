71abfe6b48e87ce2f3a087d2b8b54b9ecbbd09ed
zStarMoon21 <hughesconnor011@gmail.com>
2026-02-19 14:58:58 +0000

    Create PathNode.java

---
+++ src/main/java/skid/krypton/manager/tunnel/PathNode.java
@@ -0,0 +1,39 @@
+package skid.krypton.manager.tunnel;
+
+import net.minecraft.util.math.BlockPos;
+
+public class PathNode implements Comparable<PathNode> {
+    public BlockPos pos;
+    public PathNode parent;
+    public double gCost; // Distance from start
+    public double hCost; // Distance to target
+    public double fCost; // Total (g + h)
+    
+    public PathNode(BlockPos pos) {
+        this.pos = pos;
+    }
+    
+    public void calculateCosts(BlockPos start, BlockPos target) {
+        this.gCost = start == null ? 0 : Math.sqrt(start.getSquaredDistance(pos));
+        this.hCost = target == null ? 0 : Math.sqrt(target.getSquaredDistance(pos));
+        this.fCost = gCost + hCost;
+    }
+    
+    @Override
+    public int compareTo(PathNode other) {
+        return Double.compare(this.fCost, other.fCost);
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        PathNode node = (PathNode) obj;
+        return pos.equals(node.pos);
+    }
+    
+    @Override
+    public int hashCode() {
+        return pos.hashCode();
+    }
+}
