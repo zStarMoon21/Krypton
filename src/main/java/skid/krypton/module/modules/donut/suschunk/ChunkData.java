95cf1325f736543e8336f43dd5e4bea000936570
zStarMoon21 <hughesconnor011@gmail.com>
2026-02-20 15:01:25 +0000

    Update ChunkData.java

--- src/main/java/skid/krypton/module/modules/donut/suschunk/ChunkData.java
+++ src/main/java/skid/krypton/module/modules/donut/suschunk/ChunkData.java
@@ -1,10 +1,6 @@
 package skid.krypton.module.modules.donut.suschunk;
 
 import net.minecraft.util.math.ChunkPos;
-import net.minecraft.text.Text;
-
-import java.util.HashSet;
-import java.util.Set;
 
 public class ChunkData {
     private final ChunkPos pos;
@@ -12,20 +8,6 @@ public class ChunkData {
     private long firstSeenTime = 0;
     private long lastSeenTime = 0;
     private int timesSeen = 0;
-
-    // Growth tracking sets
-    private final Set<Long> tallBamboo = new HashSet<>();
-    private final Set<Long> tallKelp = new HashSet<>();
-    private final Set<Long> sugarCaneFarms = new HashSet<>();
-    private final Set<Long> amethystClusters = new HashSet<>();
-    private final Set<Long> largeDripstone = new HashSet<>();
-    private final Set<Long> longVines = new HashSet<>();
-
-    // Light tracking
-    private final Set<Long> lightSources = new HashSet<>();
-
-    // Pillar tracking
-    private final Set<Long> pillars = new HashSet<>();
 
     public ChunkData(ChunkPos pos) {
         this.pos = pos;
@@ -45,30 +27,6 @@ public class ChunkData {
         return lastSeenTime - firstSeenTime;
     }
 
-    // ===== GROWTH TRACKING METHODS =====
-    public void addTallBamboo(long pos) { tallBamboo.add(pos); }
-    public void addTallKelp(long pos) { tallKelp.add(pos); }
-    public void addSugarCaneFarm(long pos) { sugarCaneFarms.add(pos); }
-    public void addAmethystCluster(long pos) { amethystClusters.add(pos); }
-    public void addLargeDripstone(long pos) { largeDripstone.add(pos); }
-    public void addLongVine(long pos) { longVines.add(pos); }
-
-    public int getTallBambooCount() { return tallBamboo.size(); }
-    public int getTallKelpCount() { return tallKelp.size(); }
-    public int getSugarCaneFarmCount() { return sugarCaneFarms.size(); }
-    public int getAmethystClusterCount() { return amethystClusters.size(); }
-    public int getLargeDripstoneCount() { return largeDripstone.size(); }
-    public int getLongVineCount() { return longVines.size(); }
-
-    // ===== LIGHT TRACKING METHODS =====
-    public void addLightSource(long pos) { lightSources.add(pos); }
-    public int getLightSourceCount() { return lightSources.size(); }
-
-    // ===== PILLAR TRACKING METHODS =====
-    public void addPillar(long pos) { pillars.add(pos); }
-    public int getPillarCount() { return pillars.size(); }
-
-    // ===== GETTERS & SETTERS =====
     public ChunkPos getPos() { return pos; }
     public int getTotalScore() { return totalScore; }
     public void setTotalScore(int score) { this.totalScore = score; }
