27452b37e3bd8bd0e65c73eb7d828881f44e7c25
zStarMoon21 <hughesconnor011@gmail.com>
2026-02-20 15:41:30 +0000

    Update ChunkData.java

--- src/main/java/skid/krypton/module/modules/donut/suschunk/ChunkData.java
+++ src/main/java/skid/krypton/module/modules/donut/suschunk/ChunkData.java
@@ -1,25 +1,34 @@
 package skid.krypton.module.modules.donut.suschunk;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.math.ChunkPos;
 
 public class ChunkData {
     private final ChunkPos pos;
-    private int totalScore = 0;
-    private long firstSeenTime = 0;
-    private long lastSeenTime = 0;
-    private int timesSeen = 0;
+    private final long firstSeenTime;
+    private long lastSeenTime;
+    private long lastScanTime;
+    private int totalScore;
+    private int timesSeen;
+    
+    // Statistics
+    private int growthCount;
+    private int kelpGapCount;
+    private int lightCount;
+    private int pillarCount;
 
-    public ChunkData(ChunkPos pos) {
+    public ChunkData(ChunkPos pos, MinecraftClient mc) {
         this.pos = pos;
         this.firstSeenTime = System.currentTimeMillis();
         this.lastSeenTime = firstSeenTime;
+        this.lastScanTime = 0;
+        this.totalScore = 0;
+        this.timesSeen = 1;
     }
 
-    public void updateLoadTime() {
-        this.lastSeenTime = System.currentTimeMillis();
-    }
-
-    public void incrementTimesSeen() {
+    public void updateLastScanTime() {
+        this.lastScanTime = System.currentTimeMillis();
+        this.lastSeenTime = lastScanTime;
         this.timesSeen++;
     }
 
@@ -27,8 +36,25 @@ public class ChunkData {
         return lastSeenTime - firstSeenTime;
     }
 
+    public long getTimeSinceLastScan() {
+        return System.currentTimeMillis() - lastScanTime;
+    }
+
+    // Getters and setters
     public ChunkPos getPos() { return pos; }
     public int getTotalScore() { return totalScore; }
     public void setTotalScore(int score) { this.totalScore = score; }
     public int getTimesSeen() { return timesSeen; }
+    
+    public int getGrowthCount() { return growthCount; }
+    public void setGrowthCount(int count) { this.growthCount = count; }
+    
+    public int getKelpGapCount() { return kelpGapCount; }
+    public void setKelpGapCount(int count) { this.kelpGapCount = count; }
+    
+    public int getLightCount() { return lightCount; }
+    public void setLightCount(int count) { this.lightCount = count; }
+    
+    public int getPillarCount() { return pillarCount; }
+    public void setPillarCount(int count) { this.pillarCount = count; }
 }
