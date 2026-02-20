fb5aa611e9ac534f3559e83cc4d3eb2f389f79fc
zStarMoon21 <hughesconnor011@gmail.com>
2026-02-20 21:45:08 +0000

    Update SkinProtectMixin.java

--- src/main/java/skid/krypton/mixin/SkinProtectMixin.java
+++ src/main/java/skid/krypton/mixin/SkinProtectMixin.java
@@ -1,7 +1,5 @@
 package skid.krypton.mixin;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.PlayerListEntry;
@@ -11,8 +9,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import skid.krypton.module.modules.misc.SkinProtect;
-
-import java.util.Map;
 
 @Mixin(AbstractClientPlayerEntity.class)
 public class SkinProtectMixin {
@@ -29,17 +25,32 @@ public class SkinProtectMixin {
                 PlayerListEntry targetEntry = mc.getNetworkHandler().getPlayerListEntry(targetPlayer);
                 
                 if (targetEntry != null) {
-                    // Get the game profile which contains skin information
-                    GameProfile profile = targetEntry.getProfile();
-                    
-                    // Try to get the skin texture from the session service
-                    Map<MinecraftProfileTexture.Type, MinecraftProfileTexture> textures = 
-                        mc.getSkinProvider().getTextures(profile);
-                    
-                    if (textures.containsKey(MinecraftProfileTexture.Type.SKIN)) {
-                        MinecraftProfileTexture skinTexture = textures.get(MinecraftProfileTexture.Type.SKIN);
-                        Identifier skinId = mc.getSkinProvider().loadSkin(skinTexture, MinecraftProfileTexture.Type.SKIN);
-                        cir.setReturnValue(skinId);
+                    // In Yarn mappings, it's just getSkinTexture() on PlayerListEntry
+                    Identifier skinTexture = targetEntry.getSkinTexture();
+                    if (skinTexture != null) {
+                        cir.setReturnValue(skinTexture);
+                    }
+                }
+            }
+        }
+    }
+    
+    @Inject(method = "getModel", at = @At("HEAD"), cancellable = true)
+    private void onGetModel(CallbackInfoReturnable<String> cir) {
+        SkinProtect skinProtect = SkinProtect.getInstance();
+        
+        if (skinProtect != null && skinProtect.isEnabled()) {
+            String targetPlayer = skinProtect.getTargetPlayer();
+            MinecraftClient mc = MinecraftClient.getInstance();
+            
+            if (mc.getNetworkHandler() != null) {
+                PlayerListEntry targetEntry = mc.getNetworkHandler().getPlayerListEntry(targetPlayer);
+                
+                if (targetEntry != null) {
+                    // getModel() exists in PlayerListEntry in Yarn
+                    String model = targetEntry.getModel();
+                    if (model != null) {
+                        cir.setReturnValue(model);
                     }
                 }
             }
